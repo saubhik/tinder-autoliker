92df4098299b3964a4f63d06f62a308f399d9e62
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2018-10-10 12:23:30 +0530

    Added comments

--- autoliker.js
+++ autoliker.js
@@ -1,7 +1,10 @@
+// Paste this in your browser's DOM explorer while on the tinder website
+// To open DOM explorer, use Cmd+Option+i (macOS) or Ctr+Shift+i (Windows)
 a = setInterval(
     function(){
         var elem = document.getElementsByClassName("recsGamepad__button--like");
         elem[0].click()
-    }, 
+    },
+    // adding a 3 second time gap between successive lags
     3000
 )
